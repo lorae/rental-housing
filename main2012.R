2970454e7520cebeb64948fd88b204fa4ced65ff
Lorae <10428186+lorae@users.noreply.github.com>
2025-03-03 14:30:20 -0500

    refactor: rename 2012 data folder to `2012` and add codebook

--- main2012.R
+++ main2012.R
@@ -7,17 +7,19 @@ library(dplyr)
 # https://www.census.gov/programs-surveys/rhfs/data/puf/2012/2012-codebook.html
 
 # A list of the encodings of all values 
-catvar <- read.csv("data/2012_RHFS_National_PUF_CSV_v2.1/catvar.csv")
+catvar <- read.csv("data/2012/catvar.csv")
 
 # These are the properties that were successfully interviewed. N = 2260
-interviewprops <- read.csv("data/2012_RHFS_National_PUF_CSV_v2.1/interviewprops.csv")
+interviewprops <- read.csv("data/2012/interviewprops.csv")
 
 # These are the properties that were not successfully interviewed. N = 1770
 # TODO: Evaluate reasons data are missing in-depth.
-nonintprops <- read.csv("data/2012_RHFS_National_PUF_CSV_v2.1/nonintprops.csv")
+nonintprops <- read.csv("data/2012/nonintprops.csv")
 
 # Replicate weights for standard error calculations. Non-interview rows = 0. N = 4030
-repwgt <- read.csv("data/2012_RHFS_National_PUF_CSV_v2.1/repwgt.csv")
+repwgt <- read.csv("data/2012/repwgt.csv")
 
 # Sample design information for each record. N = 4030
-sample_design <- read.csv("data/2012_RHFS_National_PUF_CSV_v2.1/sample_design.csv")
+sample_design <- read.csv("data/2012/sample_design.csv")
+
+#
